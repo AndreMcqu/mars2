2b2bad5e1d94bb85b27f37173f9d00607aa8802d
andremcqu <mcquade.andre@gmail.com>
2022-05-11 11:23:49 +0200

    I put some text beside a picture

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.fristPictrue}>
+        <Image
+          style={styles.fristPictrue}
+          source={require('./assets/rover.jpg')} />
+      </View>
+      <View style={styles.words}>
+        <Text>Le rover Opportunity</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  fristPictrue: {
+    flex: 3,
+    width: '100%',
+  },
+  smallcontainer: {
+    flex: 1,
+  },
+  words: {
+    flex: 1,
   },
 });
